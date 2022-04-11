c79dc61dbd056ba5cf6ac34af3c5a331bceb3315
abraham <abraham@newverveconsulting.com>
2022-04-11 15:12:48 +0100

    Adding global page

---
+++ src/MyLabGlobalPage.jsx
@@ -0,0 +1,20 @@
+import ForgeUI, {
+  ProjectPage,
+  Tabs,
+  Tab,
+  Text,
+  SectionMessage,
+  Fragment,
+  IssueGlance,
+  GlobalPage,
+} from '@forge/ui';
+
+export default function () {
+  return (
+    <GlobalPage>
+      <Fragment>
+        <Text>Hello, world from MyLabGlobalPage!</Text>
+      </Fragment>
+    </GlobalPage>
+  );
+};
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,24 @@
-import ForgeUI, { render, Fragment, Text, IssuePanel } from '@forge/ui';
+import ForgeUI, { render, Fragment, Text, IssuePanel, GlobalPage } from '@forge/ui';
 import MyLabProjectPage from './MyLabProjectPage';
 import MyLabIssuePage from './MyLabIssuePage';
 import MyLabGlancePage from './MyLabGlancePage';
+import MyLabGlobalPage from './MyLabGlobalPage';
+
+/**
+ * Always start with;
+ * forge login
+ * 
+ * And then to start the app if you haven't already done it before:
+ * Run forge register to register a new copy of this app to your developer account
+ * Run npm install to install your dependencies
+ * Run forge deploy to deploy the app into the default environment
+ * Run forge install and follow the prompts to install the app -OR- forge install --upgrade
+ *  * No need to repeat the above if already done
+ * 
+ * forge tunnel to see changes live after install (needs docker to be running)!
+ * 
+ * and can use "forge logs" to debug
+ */
 
 export const Project = render(
   <MyLabProjectPage />
@@ -13,4 +30,8 @@ export const Glance = render(
 
 export const Issue = render(
   <MyLabIssuePage />
+);
+
+export const Global = render(
+  <MyLabGlobalPage />
 );
