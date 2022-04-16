4b3983d124841695d7688d5ed26729508923d952
abraham <abraham@newverveconsulting.com>
2022-04-16 22:06:17 +0100

    added an api call to get this party started

--- src/MyLabGlobalPage.jsx
+++ src/MyLabGlobalPage.jsx
@@ -7,13 +7,45 @@ import ForgeUI, {
   Fragment,
   IssueGlance,
   GlobalPage,
+  useAction,
+  useState,
+  useEffect,
 } from '@forge/ui';
+import api, { route } from '@forge/api';
+
+const getAllIssues = async () => {
+  const allProjects = 'project is not EMPTY';
+  const KTProject = 'project = "KT"';
+  const result = await api
+    .asApp()
+    .requestJira(
+      route`/rest/api/3/search?jql=${allProjects}`
+    );
+
+
+  const json = await result.json();
+  console.log(json);
+  return json;
+};
 
 export default function () {
+  const [allIssues, setAllIssues] = useState('');
+
+  useEffect(async () => {
+    const allIssues = await getAllIssues();
+
+    setAllIssues(allIssues);
+  }, []);
+
+  // setAllIssues(getAllIssues());
+  console.log('hello there!');
+  console.log(allIssues);
+
   return (
     <GlobalPage>
       <Fragment>
         <Text>Hello, world from MyLabGlobalPage!</Text>
+        <Text>{JSON.stringify(allIssues, null, 2)}</Text>
       </Fragment>
     </GlobalPage>
   );
