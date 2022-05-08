2e28468a8cfdbb93a9f14774109f239112eed8d4
abraham <abraham@newverveconsulting.com>
2022-05-08 21:43:50 +0100

    further playing with backend

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,6 @@
 import ForgeUI, { render, Fragment, Text, IssuePanel, GlobalPage } from '@forge/ui';
+import api, { route } from '@forge/api';
+import Resolver from '@forge/resolver';
 import MyLabProjectPage from './MyLabProjectPage';
 import MyLabIssuePage from './MyLabIssuePage';
 import MyLabGlancePage from './MyLabGlancePage';
@@ -35,3 +37,37 @@ export const Issue = render(
 export const Global = render(
   <MyLabGlobalPage />
 );
+
+const resolver = new Resolver();
+
+const getAllIssues = async () => {
+  const allProjects = 'project is not EMPTY';
+  const startAt = 0;
+  const maxResults = 50;
+  let count = 0;
+  const paginated = `&startAt=${startAt}&maxResults=${maxResults}`;
+  const KTProject = 'project = "KT"';
+  const result = await api
+    .asApp()
+    .requestJira(
+      route`/rest/api/3/search?jql=project is not EMPTY&startAt=0&maxResults=10&fields=summary,comment`
+      // route`/rest/api/3/search?jql=${allProjects}` // ${paginated}&fields=summary,comment`
+    );
+
+
+  const json = await result.json();
+  // console.log(json);
+  // setAllIssues(JSON.stringify(json, null, 2));
+  return json;
+};
+
+resolver.define('getText', async (req) => {
+  console.log(req);
+  const resp = await getAllIssues();
+  // return resp;
+  return JSON.stringify(resp, null, 2);
+
+  // return 'Hello, this is some data for the world!';
+});
+
+export const handler = resolver.getDefinitions();
--- static/hello-world/src/App.js
+++ static/hello-world/src/App.js
@@ -1,24 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { invoke } from '@forge/bridge';
 
-const reTryCatch = async (output, setOutput) => {
-  // let output;
-  let count = 0;
-  const maxTries = 3;
-  while (count < maxTries) {
-    setOutput(`running attempt ${count + 1}...`);
-    console.log(`running attempt ${count + 1}...`);
-    // const result = await api
-    //   .asApp()
-    //   .requestJira(
-    //     route`/rest/api/3/search?jql=${allProjects}${paginated}&fields=summary,comment`
-    //   );
-    await new Promise((r) => setTimeout(r, 1000));
-    count = count+1;
-  }
-  setOutput(`finished at ${count + 1} attempts`);
-};
-
 function App() {
   const [data, setData] = useState(null);
   const [allIssues, setAllIssues] = useState('loading...');
@@ -27,21 +9,17 @@ function App() {
     invoke('getText', { example: 'my-invoke-variable' }).then(setData);
   }, []);
 
-  useEffect(() => {
-      // const allIssues = 
-      // await getAllIssues(allIssues, setAllIssues);
-      reTryCatch(allIssues, setAllIssues);
-  
-      // setAllIssues(allIssues);
-    }, []);
+  const keys = data 
+    ? (JSON.parse(data).issues.map((issue) => issue.key).join(', '))
+    : 'Loading...';
 
   return (
     <div>
-      <h2>There is going to be data here:</h2>
-      {data ? data : 'Loading...'}
-      <div>
+      <h2>Hello! There is going to be data here:</h2>
+      {keys}
+      {/* <div>
         {allIssues}
-      </div>
+      </div> */}
     </div>
   );
 }
