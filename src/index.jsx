c12ce517669fd247ad0c7686393b7882fde325d4
abraham <abraham@newverveconsulting.com>
2022-05-09 13:27:58 +0100

    npm-watch added for atobuild when changes detected + adventure continues with objects and arrays

--- src/index.jsx
+++ src/index.jsx
@@ -22,6 +22,46 @@ import MyLabGlobalPage from './MyLabGlobalPage';
  * and can use "forge logs" to debug
  */
 
+ const resolver = new Resolver();
+
+ const getAllIssues = async (startAt = 0, maxResults = 3) => {
+   const allProjects = 'project is not EMPTY';
+  //  const startAt = 0;
+  //  const maxResults = 50;
+   let count = 0;
+   const paginated = `&startAt=${startAt}&maxResults=${maxResults}`;
+   const KTProject = 'project = "KT"';
+   const result = await api
+     .asApp()
+     .requestJira(
+       route`/rest/api/3/search?jql=project is not EMPTY&startAt=${startAt}&maxResults=${maxResults}&fields=summary,comment`
+       // route`/rest/api/3/search?jql=${allProjects}` // ${paginated}&fields=summary,comment`
+     );
+ 
+   console.log('startAt', startAt);
+   console.log('maxResults', maxResults);
+   console.log('result', result);
+   const json = await result.json();
+   console.log('json', json);
+   // console.log(json);
+   // setAllIssues(JSON.stringify(json, null, 2));
+   return json;
+ };
+ 
+ resolver.define('getText', async (req) => {
+   console.log(req);
+   const { startAt, maxResults } = req.payload;
+   console.log('startAt', startAt);
+   console.log('maxResults', maxResults);
+   const resp = await getAllIssues(startAt, maxResults);
+   // return resp;
+   return JSON.stringify(resp, null, 2);
+ 
+   // return 'Hello, this is some data for the world!';
+ });
+ 
+ export const handler = resolver.getDefinitions();
+
 export const Project = render(
   <MyLabProjectPage />
 );
@@ -37,37 +77,3 @@ export const Issue = render(
 export const Global = render(
   <MyLabGlobalPage />
 );
-
-const resolver = new Resolver();
-
-const getAllIssues = async () => {
-  const allProjects = 'project is not EMPTY';
-  const startAt = 0;
-  const maxResults = 50;
-  let count = 0;
-  const paginated = `&startAt=${startAt}&maxResults=${maxResults}`;
-  const KTProject = 'project = "KT"';
-  const result = await api
-    .asApp()
-    .requestJira(
-      route`/rest/api/3/search?jql=project is not EMPTY&startAt=0&maxResults=10&fields=summary,comment`
-      // route`/rest/api/3/search?jql=${allProjects}` // ${paginated}&fields=summary,comment`
-    );
-
-
-  const json = await result.json();
-  // console.log(json);
-  // setAllIssues(JSON.stringify(json, null, 2));
-  return json;
-};
-
-resolver.define('getText', async (req) => {
-  console.log(req);
-  const resp = await getAllIssues();
-  // return resp;
-  return JSON.stringify(resp, null, 2);
-
-  // return 'Hello, this is some data for the world!';
-});
-
-export const handler = resolver.getDefinitions();
--- static/hello-world/src/App.js
+++ static/hello-world/src/App.js
@@ -2,21 +2,49 @@ import React, { useEffect, useState } from 'react';
 import { invoke } from '@forge/bridge';
 
 function App() {
-  const [data, setData] = useState(null);
-  const [allIssues, setAllIssues] = useState('loading...');
+  const [data, setData] = useState(undefined);
+  const [allIssues, setAllIssues] = useState([1, 2].join(', '));
 
-  useEffect(() => {
-    invoke('getText', { example: 'my-invoke-variable' }).then(setData);
+  useEffect(async () => {
+    let data;
+    let isFinished = false;
+    let currentData = {...data} || {};
+    let currentIssues = [...allIssues] || [];
+    let startAt = 0;
+    const maxResults = 3;
+    while (!isFinished) {
+      
+      // invoke('getText', { startAt: 0, maxResults: 3 }).then(setData);
+      data = await invoke('getText', { startAt: startAt, maxResults });
+      const jsonData = JSON.parse(data);
+      console.log('data', data);
+      console.log('jsonData', jsonData);
+      setData({...currentData, jsonData});
+      setAllIssues(jsonData.issues.map((issue) => issue.key).join(', '));
+      // setData(jsonData);
+      isFinished = true;
+      if (data.length === 0) isFinished = true;
+    }
+    // invoke('getText', { startAt: 0, maxResults: 3 }).then(setData);
   }, []);
 
-  const keys = data 
-    ? (JSON.parse(data).issues.map((issue) => issue.key).join(', '))
-    : 'Loading...';
+  if (data){
+    console.log('data.issues', data.issues);
+  }
+  
+
+  // const keys = data 
+  //   ? (data.issues.map((issue) => issue.key).join(', '))
+  //   : 'Loading...';
 
   return (
     <div>
       <h2>Hello! There is going to be data here:</h2>
-      {keys}
+      {/* {keys} */}
+      {allIssues}
+      <br />
+      <br />
+      {JSON.stringify(data)}
       {/* <div>
         {allIssues}
       </div> */}
